3ec925ecfccf9483742b92c1fda5794b4219070d
Roy Xiang <git@royxiang.me>
2024-08-12 21:08:15 +0800

    feat(route/nyaa): 调整标题并略微修改格式

--- lib/routes/nyaa/main.ts
+++ lib/routes/nyaa/main.ts
@@ -1,4 +1,4 @@
-import { Route } from '@/types';
+import { Data, DataItem, Route } from '@/types';
 import { config } from '@/config';
 import Parser from 'rss-parser';
 
@@ -20,10 +20,17 @@ export const route: Route = {
     handler,
 };
 
-async function handler(ctx) {
-    const parser = new Parser({
+interface NyaaFields {
+    magnet: string;
+    infoHash: string;
+    categoryId: string;
+}
+type NyaaItem = DataItem & NyaaFields;
+
+async function handler(ctx): Promise<Data> {
+    const parser = new Parser<Data, NyaaItem>({
         customFields: {
-            item: ['magnet', ['nyaa:infoHash', 'infoHash']],
+            item: ['magnet', ['nyaa:infoHash', 'infoHash'], ['nyaa:categoryId', 'categoryId'], ['nyaa:category', 'category', { keepArray: true }]],
         },
         headers: {
             'User-Agent': config.ua,
@@ -36,28 +43,39 @@ async function handler(ctx) {
 
     let currentRSSURL = `${rootURL}/?page=rss`;
     let currentLink = `${rootURL}/`;
+    const currentTitle: string[] = [];
     if (username !== undefined) {
         currentRSSURL = `${currentRSSURL}&u=${encodeURI(username)}`;
         currentLink = `${currentLink}user/${encodeURI(username)}`;
+        currentTitle.push(`[${username}]`);
     }
     if (query !== undefined) {
         currentRSSURL = `${currentRSSURL}&q=${encodeURI(query)}`;
         currentLink = `${currentLink}?q=${encodeURI(query)}`;
+        currentTitle.push(query);
+    }
+    if (currentTitle.length === 0) {
+        currentTitle.push('Home');
     }
 
     const feed = await parser.parseURL(currentRSSURL);
 
     feed.items.map((item) => {
+        const magnetUri = `magnet:?xt=urn:btih:${item.infoHash}`;
         item.description = item.content;
-        item.enclosure_url = `magnet:?xt=urn:btih:${item.infoHash}`;
+        item.category?.push(item.categoryId);
+        item.enclosure_url = rootURL === 'https://nyaa.si' ? item.link : magnetUri;
         item.enclosure_type = 'application/x-bittorrent';
+        item.link = item.guid;
+        item.guid = magnetUri;
         return item;
     });
 
     return {
-        title: feed.title,
+        title: `Nyaa - ${currentTitle.join(' ')} - Torrent File RSS`,
         link: currentLink,
         description: feed.description,
         item: feed.items,
+        allowEmpty: true,
     };
 }
